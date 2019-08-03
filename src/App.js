9e1697b4de9763ac5d8eba365f66fd5e7c5e54a4
nana <nana@runapptechnologies.com>
2019-08-03 02:44:09 +0000

    basic route setup

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React from 'react';
 import { Router, Route, Switch } from 'react-router-dom';
 
 import history from './history';
+import appRoutes from './app-routes';
+import Dashboard from './components/dashboard';
 
 const App = () => (
   <Router history={history}>
     <Switch>
-      <Route path="/" component={() => <div>Base set up</div>} />
+      <Route
+        path="/"
+        render={() => (
+          <React.Fragment>
+            <Route path={appRoutes.home} component={() => <div>Home</div>} />
+            <Route path={appRoutes.dashboard} component={Dashboard} />
+          </React.Fragment>
+        )}
+      />
     </Switch>
   </Router>
 );
