5c7ceb1258649a7ec5559af8f2798acc8a8e77b3
nana <nana@runapptechnologies.com>
2019-08-03 14:33:14 +0000

    added menus to side bar

---
+++ src/components/side-bar/MenuDefinition.js
@@ -0,0 +1,14 @@
+import routes from '../../app-routes';
+
+export default [
+  {
+    name: 'home',
+    label: 'Home',
+    route: routes.home
+  },
+  {
+    name: 'dashboard',
+    label: 'Dashboard',
+    route: routes.dashboard
+  }
+];
--- src/components/side-bar/SideBarComponent.js
+++ src/components/side-bar/SideBarComponent.js
@@ -2,10 +2,18 @@ import React from 'react';
 
 import './SideBarComponent.css';
 
+import MenuDefinition from './MenuDefinition';
+
 const SideBarComponent = () => (
   <div className="side-bar ">
     <div className="side-bar__header">Side Bar header</div>
-    <div className="side-bar__content">Side Bar Content</div>
+    <div className="side-bar__content">
+      <div>
+        {MenuDefinition.map(menu => (
+          <div key={menu.name}>{menu.label}</div>
+        ))}
+      </div>
+    </div>
   </div>
 );
 
